9ebca509bab4e4335035b23ea4f1417b252c7b74
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-07-07 22:33:01 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
-import { Button } from "@/components/ui/button"
-import LandingPage from "./components/pages/landingPage"
-
+import Navbar from "./components/navbar";
+import Home from "./components/pages/Home";
+import Footer from "./components/Footer";
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
 function App() {
+
+  const Layout = ()=>{
+    return(
+      <div className="main">
+        <Navbar/>
+        <div className="container">
+          <div className="menuContainer">
+            {/* <Menu/> */}
+          </div>
+          <div className="contentContainer">
+            {/* <Outlet/> */}
+          </div>
+        </div>
+        <Footer/>
+      </div>
+    )
+  }
+
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    children:[
+      {
+        path: "/",
+        element:<Home/>,
+      },
+    ]
+  },
+]);
+
   return (
-    <LandingPage></LandingPage>
+    <RouterProvider router={router} />
   )
 }
 
