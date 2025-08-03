2a8a0d798c6111b2e799c070c375f608df7289e4
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-08-04 04:27:34 +0600

    updated with router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,41 @@
-import LearningCardGrid from './components/LearningCardGrid'
-import Logo from './components/LogoSection'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import LearningCardGrid from './components/Main Website/Home/LearningCardGrid'
+import Logo from './components/Main Website/Home/LogoSection'
 import VisionSection from './components/OurVision'
-import StatsSection from './components/stats'
+import StatsSection from './components/Main Website/Home/stats'
 import Footer from './components/Footer'
-import Event from './components/FeaturedEventsCarousel'
+import Event from './components/Main Website/Home/FeaturedEventsCarousel'
 import Blog from './components/BlogCard'
-import TopPerformers from './components/TopPerformers'
+import TopPerformers from './components/Main Website/Home/TopPerformers'
 import Navbar from './components/Navbar'
-import HeroCarousel from './components/HeroCarousel'
+import HeroCarousel from './components/Main Website/Home/HeroCarousel'
+import AboutIEEE from './components/Main Website/IEEE/IEEE';
+
+const HomePage = () => (
+  <>
+    <HeroCarousel/>
+    <LearningCardGrid/>
+    <Logo/>
+    <VisionSection/>
+    <StatsSection/>
+    <Event/>
+    <TopPerformers/>
+    <Blog/>
+  </>
+);
 
 const App = () => {
   return (
-    <>
-      <Navbar/>
-      <HeroCarousel/>
-      <LearningCardGrid/>
-      <Logo/>
-      <VisionSection/>
-      <StatsSection/>
-      <Event/>
-      <TopPerformers/>
-      <Blog/>
-      <Footer/>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about/IEEE" element={<AboutIEEE />} />
+      </Routes>
+      <Footer />
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
