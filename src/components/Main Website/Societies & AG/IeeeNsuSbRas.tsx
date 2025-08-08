190c7c85e578ba3dced5d12e0ba31d500062d2e7
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-08-08 16:05:44 +0600

    updated title with useEffect

--- src/components/Main Website/Societies & AG/IeeeNsuSbRas.tsx
+++ src/components/Main Website/Societies & AG/IeeeNsuSbRas.tsx
@@ -1,5 +1,11 @@
+import { useEffect } from "react";
 
 const IeeeNsuSbRas = () => {
+
+  useEffect(() => {
+    document.title = "IEEE NSU RAS SBC";
+  }, []);
+
   return (
     <div>
       <p className='font-bold'>HANDLE WITH CARE</p>
