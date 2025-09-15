de3613e75c71fc372cfa1a6a61eef9cc89ef58e5
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-09-16 02:33:55 +0600

    alignment fixed

--- src/components/ui/section-heading.tsx
+++ src/components/ui/section-heading.tsx
@@ -6,10 +6,10 @@ interface SectionHeadingProps {
 const SectionHeading: React.FC<SectionHeadingProps> = ({ title, widthClass }) => {
   return (
     <>
-      <h2 className="max-w-[1078px] mx-auto text-ieee-darkblue-75 text-3xl font-bold px-4 mt-10">
+      <h2 className="max-w-[1000px] mx-auto text-ieee-darkblue-75 text-3xl font-bold mt-10 max-lg:px-5">
         {title}
       </h2>
-      <div className="flex gap-1 max-w-[1045px] mx-auto mt-2 mb-4 max-md:ml-4">
+      <div className="flex gap-1 max-w-[1000px] mx-auto mt-2 mb-4 max-md:ml-4 max-sm:px-1">
         <div className={`h-1 ${widthClass} bg-ieee-darkblue-75 rounded-xs`}></div>
         <div className="h-1 w-2 bg-ieee-darkblue-75 rounded-xs"></div>
         <div className="h-1 w-2 bg-ieee-darkblue-75 rounded-xs"></div>
