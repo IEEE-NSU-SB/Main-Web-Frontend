b8ddd06d69d4aa05c192abfbd7b36c07c9ef72ab
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-09-16 03:57:27 +0600

    achievements page added

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import About from '@/pages/about/pages[id]/pages';
 
 import JoinINSB from '@/pages/get-involved/join-insb'
 import AllMembers from '@/pages/members/all-members-statistcs'
+import Achievements from './pages/activities/achievements';
 
 import "odometer/themes/odometer-theme-default.css";
 const App = () => {
@@ -18,10 +19,18 @@ const App = () => {
     <Router>
       <Navbar />
       <Routes>
+        {/* Home */}
         <Route path="/" element={<HomePage />} />
-        <Route path="/about/:id" element={<About />} />
+        {/* Activities */}
+        <Route path="/achievements" element={<Achievements />} />
+        {/* Societies & AG */}
         <Route path="/society-ag/:id" element={<SocietyOrAg />} />
+        {/* Members  */}
         <Route path="/all-members" element={<AllMembers/>} />
+        {/* About  */}
+        <Route path="/about/:id" element={<About />} />
+        {/* Publications  */}
+        {/* Get Involved  */}
         <Route path="/join-ieee-nsu-sb" element={<JoinINSB recruitSession={{
     session_end_date_time: '2025-08-20T23:59:59Z',
     recruitment_event_link: 'https://facebook.com/ieeensu/events/xyz',
