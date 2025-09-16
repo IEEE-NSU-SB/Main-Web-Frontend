400f4b5349ef60e7d6ad3c761d3165f2279aad9c
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-09-16 14:33:14 +0600

    formatted

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -1,14 +1,12 @@
 // components/Skeleton.tsx
-import React from 'react';
+import React from "react";
 
 interface SkeletonProps {
   className?: string;
 }
 
 const Skeleton: React.FC<SkeletonProps> = ({ className }) => {
-  return (
-    <div className={`animate-pulse bg-gray-300 rounded ${className}`} />
-  );
+  return <div className={`animate-pulse bg-gray-300 rounded ${className}`} />;
 };
 
 export default Skeleton;
