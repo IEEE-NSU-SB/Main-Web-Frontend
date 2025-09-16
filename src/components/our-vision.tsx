2764577758245029888deb07517a762d21e408df
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-09-16 14:35:34 +0600

    fade in animation added

--- src/components/our-vision.tsx
+++ src/components/our-vision.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import SectionHeading from "./ui/section-heading";
 import Skeleton from "@/components/skeleton";
 import visionData from "@/data/vision.json"; // Uncomment this while using local JSON
-import {useFetchDataAPI,useFetchDataJSON} from "@/hooks/fetchdata";
+import { useFetchDataAPI, useFetchDataJSON } from "@/hooks/fetchdata";
+import FadeIn from "./ui/fade-in";
 
 const VisionSection = () => {
   const [loading, setLoading] = useState(true);
@@ -35,26 +36,29 @@ const VisionSection = () => {
 
   return (
     <>
-    <SectionHeading title="Our Vision" widthClass="w-42" />
-    <section className="max-w-[1000px] mx-auto py-2 pb-16 max-lg:px-5">
-
-      {loading ? (
-        <div className="space-y-2">
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-full" />
-          <Skeleton className="h-4 w-6/12" />
-        </div>
-      ) : vision ? (
-        <p className="text-lg text-ieee-black-75 leading-relaxed text-justify">
-          {vision.text}
-        </p>
-      ) : (
-        <p className="text-red-500 px-4">Failed to load vision statement.</p>
-      )}
-    </section>
+      <FadeIn>
+        <SectionHeading title="Our Vision" widthClass="w-42" />
+        <section className="max-w-[1000px] mx-auto py-2 pb-16 max-lg:px-5">
+          {loading ? (
+            <div className="space-y-2">
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-full" />
+              <Skeleton className="h-4 w-6/12" />
+            </div>
+          ) : vision ? (
+            <p className="text-lg text-ieee-black-75 leading-relaxed text-justify">
+              {vision.text}
+            </p>
+          ) : (
+            <p className="text-red-500 px-4">
+              Failed to load vision statement.
+            </p>
+          )}
+        </section>
+      </FadeIn>
     </>
   );
 };
