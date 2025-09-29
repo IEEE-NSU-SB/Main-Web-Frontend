a1d3d7e41454cebda7ecaae3f0c4ad34b1edceca
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-09-29 23:14:06 +0600

    toolkit added

---
+++ src/pages/publications/toolkit/pages.tsx
@@ -0,0 +1,12 @@
+import Wave from "@/components/wave"
+
+const pages = () => {
+  return (
+    <>
+      <Wave title="Toolkit"></Wave>
+      <h3>Toolkit</h3>
+    </>
+  )
+}
+
+export default pages
