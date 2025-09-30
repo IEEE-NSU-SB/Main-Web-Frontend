6a05a285edbf26dd14891aafb21445de0b0a1b68
shohan12727 <aishohan001@gmail.com>
2025-09-30 18:52:39 +0600

    fade in added in scag mission vision

--- src/pages/society-and-ag/pages[id]/ScAgMissionVision.tsx
+++ src/pages/society-and-ag/pages[id]/ScAgMissionVision.tsx
@@ -1,4 +1,5 @@
 
+import FadeIn from '@/components/ui/fade-in';
 import { use, type FC } from 'react';
 import { useParams } from 'react-router';
 
@@ -55,36 +56,39 @@ const ScAgMissionVision: FC<MissionVisionProps> = ({ missionVisionPromise }) => 
 
     return (
         <>
-            <div className="md:max-w-[1080px] w-full mx-auto my-10 px-3">
-                {
-                    missionVisionShow.map((element) => (
-                        <>
-                            {/* mission  */}
-                            <div className='flex my-3 bg-[#006CA5]  md:flex-row flex-col rounded-md border'>
-                                <div className="text-part w-full md:w-1/2 p-7  text-white text-justify">
-                                    <h1 className='text-5xl font-bold mb-7'>Mission</h1>
-                                    <p>{element.mission_description}</p>
+            <FadeIn>
+                <div className="md:max-w-[1080px] w-full mx-auto my-10 px-3">
+                    {
+                        missionVisionShow.map((element) => (
+                            <>
+                                {/* mission  */}
+                                <div className='flex my-3 bg-[#006CA5]  md:flex-row flex-col rounded-md border'>
+                                    <div className="text-part w-full md:w-1/2 p-7  text-white text-justify">
+                                        <h1 className='text-5xl font-bold mb-7'>Mission</h1>
+                                        <p>{element.mission_description}</p>
+                                    </div>
+                                    <div className="img-part w-full md:w-1/2">
+                                        <img src={element.mission_image} className='rounded-r-md w-full h-full object-cover' alt="" />
+                                    </div>
                                 </div>
-                                <div className="img-part w-full md:w-1/2">
-                                    <img src={element.mission_image} className='rounded-r-md w-full h-full object-cover' alt="" />
+                                {/* vision  */}
+                                <div className='flex my-6 bg-[#006CA5]  md:flex-row-reverse flex-col rounded-md border'>
+                                    <div className="text-part w-full md:w-1/2 p-7  text-white text-justify">
+                                        <h1 className='text-5xl font-bold mb-7'>Vision</h1>
+                                        <p>{element.vision_description}</p>
+                                    </div>
+                                    <div className="img-part w-full md:w-1/2">
+                                        <img src={element.vision_image} className='rounded-l-md w-full h-full object-cover' alt="" />
+                                    </div>
                                 </div>
-                            </div>
-                  {/* vision  */}
-                            <div className='flex my-6 bg-[#006CA5]  md:flex-row-reverse flex-col rounded-md border'>
-                                <div className="text-part w-full md:w-1/2 p-7  text-white text-justify">
-                                    <h1 className='text-5xl font-bold mb-7'>Vision</h1>
-                                    <p>{element.vision_description}</p>
-                                </div>
-                                <div className="img-part w-full md:w-1/2">
-                                    <img src={element.vision_image} className='rounded-l-md w-full h-full object-cover' alt="" />
-                                </div>
-                            </div>
 
 
-                        </>
-                    ))
-                }
-            </div>
+                            </>
+                        ))
+                    }
+                </div>
+            </FadeIn>
+
         </>
     );
 };
