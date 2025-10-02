73aa0c3181ca87bef324912f7b54b5c5cdfd05cd
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-10-02 12:42:48 +0600

    welcome page added

--- src/pages/welcome/page.tsx
+++ src/pages/welcome/page.tsx
@@ -1,7 +1,10 @@
+import INSB from "../../assets/logo/insb.gif";
+
 const welcomePage = () => {
   return (
     <div>
       <h2>Welcome-Page</h2>
+      <img src={INSB} alt="" />
     </div>
   )
 }
