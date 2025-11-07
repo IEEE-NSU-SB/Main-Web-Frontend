4fed650c9a6eb13b51aa8720121e4e8eaab17b67
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-11-07 20:22:21 +0600

    updated

--- src/pages/about/ieee-nsu-sb/Page.tsx
+++ src/pages/about/ieee-nsu-sb/Page.tsx
@@ -1,6 +1,9 @@
 import Wave from "@/components/Wave"
+import { useFetchDataJSON } from "@/hooks/fetchdata"
+
 
 const AboutIeeeNsuSb = () => {
+path:"pages/about/ieee-nsu-sb/data.json"
   return (
     <>
     <Wave title="IEEE NSU Student Branch"/>
