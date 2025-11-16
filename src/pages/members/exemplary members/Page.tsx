aabe1fb08b76cc19863e6e022352c8a7e2b60409
Arman Mokammel <armanmokammel@gmail.com>
2025-11-17 05:51:51 +0600

    Exemplary members api added

--- src/pages/members/exemplary members/Page.tsx
+++ src/pages/members/exemplary members/Page.tsx
@@ -1,63 +1,20 @@
+import { useFetchDataAPI } from "@/hooks/fetchdata";
 import Members from "./members";
 
-const ExemplaryMember = [
-  {
-    member_name: "John Doe",
-    member_picture: "/photos/john.png",
-    former_position: "Chair",
-    activity_year: "2021",
-    current_activity: "Software Engineer",
-    facebook_account_link: "https://facebook.com",
-    email: "john@example.com",
-    achievements: "<ul><li>Award X</li><li>Project Lead</li></ul>",
-    rank:"1",
-  },
-  {
-    member_name: "John Doe",
-    former_position: "Chair",
-    activity_year: "2021",
-    current_activity: "Software Engineer",
-    member_picture: "/photos/john.png",
-    facebook_account_link: "https://facebook.com",
-    email: "john@example.com",
-    achievements: "<ul><li>Award X</li><li>Project Lead</li></ul>",
-    rank:"2",
-  },
-  {
-    member_name: "John Doe",
-    former_position: "Chair",
-    activity_year: "2021",
-    current_activity: "Software Engineer",
-    member_picture: "/photos/john.png",
-    facebook_account_link: "https://facebook.com",
-    email: "john@example.com",
-    achievements: "<ul><li>Award X</li><li>Project Lead</li></ul>",
-    rank:"3",
-  },
-  {
-    member_name: "John Doe",
-    former_position: "Chair",
-    activity_year: "2021",
-    current_activity: "Software Engineer",
-    member_picture: "/photos/john.png",
-    facebook_account_link: "https://facebook.com",
-    email: "john@example.com",
-    achievements: "<ul><li>Award X</li><li>Project Lead</li></ul>",
-    rank:"4",
-  },
-  {
-    member_name: "John Doe",
-    former_position: "Chair",
-    activity_year: "2021",
-    current_activity: "Software Engineer",
-    member_picture: "/photos/john.png",
-    facebook_account_link: "https://facebook.com",
-    email: "john@example.com",
-    achievements: "<ul><li>Award X</li><li>Project Lead</li></ul>",
-    rank:"5",
-  },
-];
+interface Member {
+  member_name: string;
+  former_position: string;
+  activity_year: string;
+  current_activity: string;
+  member_picture?: string;
+  facebook_account_link?: string;
+  email?: string;
+  achievements: string;
+  rank:string;
+}
 
 export default function Exemplary() {
-  return <Members members={ExemplaryMember} />;
+  const { data } = useFetchDataAPI<Member[]>({ apiUrl: `main_website/get_exemplary_members/` });
+
+  return <Members members={data || []} />;
 }
