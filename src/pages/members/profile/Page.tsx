cccd05849ea83fce5a8a30d042c49e9cc4154851
0xTARIF <tarifbinmehedicrp@gmail.com>
2025-12-13 01:01:21 +0600

    blog Condition

--- src/pages/members/profile/Page.tsx
+++ src/pages/members/profile/Page.tsx
@@ -5,21 +5,19 @@ import { useParams } from "react-router-dom";
 import { useFetchDataAPI } from "@/hooks/fetchdata";
 
 const MemberProfile: React.FC = () => {
-
   const { id } = useParams();
 
   const DEFAULT_IMAGE = "/default_profile_picture.png";
 
   const { loading, data, error } = useFetchDataAPI<any>({
-      apiUrl: `main_website/get_member_details/${id}/`,
-    });
-    
-  if (!loading && !error) {
+    apiUrl: `main_website/get_member_details/${id}/`,
+  });
 
+  if (!loading && !error) {
     return (
       <>
         <Wave title="Member Profile" subtitle={`${data.name}`} />
-  
+
         <div className="max-w-[1080px] mx-auto px-4 py-10 space-y-10">
           {/* Top Section */}
           <div className="flex flex-col md:flex-row justify-between items-center md:items-start gap-6">
@@ -28,12 +26,12 @@ const MemberProfile: React.FC = () => {
                 <img
                   className="w-full h-full hover:scale-105 object-cover transition-all duration-300 cursor-pointer"
                   src={
-                    data.image ==  null || '' ? DEFAULT_IMAGE : data.image
+                    !data.image || data.image === "" ? DEFAULT_IMAGE : data.image
                   }
                   alt={data.name}
                 />
               </div>
-  
+
               <div className="flex flex-col items-center md:items-start">
                 <h1 className="text-[20px] md:text-[26px] shine-text">
                   {data.name}
@@ -49,7 +47,7 @@ const MemberProfile: React.FC = () => {
                     Recruitment Session : {data.recruitmentSession}
                   </p>
                 )}
-  
+
                 <div className="flex gap-3 mt-3 text-gray-700 text-lg">
                   {data.facebook && (
                     <a href={data.facebook} target="_blank">
@@ -69,67 +67,94 @@ const MemberProfile: React.FC = () => {
                 </div>
               </div>
             </div>
-  
+
             {/* Achievements */}
             {data.achievements.length > 0 && (
-              <div className="border-2 border-ieee-darkyellow rounded-border-ieee-darkyellow orange border-[2px_2px] rounded-[6px_20px] w-full md:w-80 bg-amber-200 overflow-hidden shadow-[2px_2px_8px_var(--color-ieee-gray-50)] w-full md:w-80 bg-amber-200 shadow">
+              <div className="border-2 border-ieee-darkyellow rounded-border-ieee-darkyellow orange border-[2px_2px] rounded-[6px_20px] w-full md:w-80 bg-amber-200 overflow-hidden shadow-[2px_2px_8px_var(--color-ieee-gray-50)]">
                 <h2 className="font-semibold px-4 py-2">Achievements</h2>
                 <ul className="text-sm text-gray-600 space-y-3 max-h-29 overflow-y-auto p-2 bg-white ieee-scrollbar">
-                  {data.achievements.map((ach: {title: string; tenure: string}, i:React.Key) => (
-                    <li
-                      key={i}
-                      className="flex flex-col gap-1 border-b pb-2 last:border-none"
-                    >
-                      <span className="flex items-center gap-2">
-                        ⭐{" "}
-                        <span className="font-medium">
-                          {ach.title} ({ach.tenure})
+                  {data.achievements.map(
+                    (ach: { title: string; tenure: string }, i: React.Key) => (
+                      <li
+                        key={i}
+                        className="flex flex-col gap-1 border-b pb-2 last:border-none"
+                      >
+                        <span className="flex items-center gap-2">
+                          ⭐{" "}
+                          <span className="font-medium">
+                            {ach.title} ({ach.tenure})
+                          </span>
                         </span>
-                      </span>
-                    </li>
-                  ))}
+                      </li>
+                    )
+                  )}
                 </ul>
               </div>
             )}
           </div>
-  
+
           {/* About Me */}
           <div className="rounded-lg p-6 space-y-6 shadow-[2px_2px_8px_var(--color-ieee-gray-50)] border-black/50 border-1">
             <div>
               <h2 className="text-[20px] font-semibold mb-2">About Me</h2>
+
               <p className="text-[16px] text-gray-700">
                 IEEE Mail: {data.ieeeEmail}
               </p>
+
               <p className="text-[16px] text-gray-700">
                 NSU Mail: {data.nsuEmail}
               </p>
-              <p className="text-[16px] text-gray-700">
-                Blood Group: {data.bloodGroup}
-              </p>
+
+              {data.bloodGroup &&
+                data.bloodGroup.toLowerCase() !== "none" && (
+                  <p className="text-[16px] text-gray-700">
+                    Blood Group: {data.bloodGroup}
+                  </p>
+                )}
             </div>
-  
+
             <div className="grid md:grid-cols-2 gap-6">
-              {data.roles.map((role: {organization: string, currentPosition: string, team: string, tenure: string, current: boolean}, i: React.Key) => (
-                <div key={i}>
-                  <div className="flex items-center gap-2 mb-1">
-                    <div className="w-3 h-3 rounded-full bg-gradient-to-r from-ieee-blue to-ieee-cyan rotate-45"></div>
-                    <h3 className="font-bold text-[22px]">{role.organization}</h3>
+              {data.roles.map(
+                (
+                  role: {
+                    organization: string;
+                    currentPosition: string;
+                    team: string;
+                    tenure: string;
+                    current: boolean;
+                  },
+                  i: React.Key
+                ) => (
+                  <div key={i}>
+                    <div className="flex items-center gap-2 mb-1">
+                      <div className="w-3 h-3 rounded-full bg-gradient-to-r from-ieee-blue to-ieee-cyan rotate-45"></div>
+                      <h3 className="font-bold text-[22px]">
+                        {role.organization}
+                      </h3>
+                    </div>
+
+                    {role.tenure && <p>Tenure: {role.tenure}</p>}
+
+                    <p>
+                      {role.current ? "Current Position" : "Position"}:{" "}
+                      {role.currentPosition}
+                    </p>
+
+                    {role.team && (
+                      <p>
+                        {role.current ? "Current Team" : "Team"}: {role.team}
+                      </p>
+                    )}
                   </div>
-                  {role.tenure && (
-                    <p>Tenure: {role.tenure}</p>
-                  )}
-                  <p>{role.current ? "Current Position": "Position"}: {role.currentPosition}</p>
-                  {role.team && (
-                    <p>{role.current ? "Current Team": "Team"}: {role.team}</p>
-                  )}               
-                </div>
-              ))}
+                )
+              )}
             </div>
           </div>
         </div>
       </>
     );
-  };
+  }
 };
 
 export default MemberProfile;
